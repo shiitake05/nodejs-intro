dfbcebf7866596394ab9c3780177fd5f10ec76bd
shiitake05 <pegtar377512@gmail.com>
2024-10-04 22:28:08 +0900

    initial commit

---
+++ ex-gen-app-05-03/routes/hello.js
@@ -0,0 +1,155 @@
+const express = require('express');
+const router = express.Router();
+
+const sqlite3 = require('sqlite3');
+
+// データベースオブジェクトの取得
+const db = new sqlite3.Database('mydb.db');
+
+// GETアクセスの処理
+router.get('/',(req, res, next) => {
+  db.serialize(() => {
+    var rows = "";
+    db.each("select * from mydata",(err, row) => {
+      if (!err) {
+        rows += "<tr><th>" + row.id + "</th><td>"
+          + row.name + "</td></tr>";
+      }   
+    }, (err, count) => {
+      if (!err){
+        var data = {
+          title: 'Hello!',
+          content: rows
+        };
+        res.render('hello/index', data);        
+      }
+    }); 
+  });
+});
+
+router.get('/add', (req, res, next) => {
+  var data = {
+      title: 'Hello/Add',
+      content: '新しいレコードを入力：'
+  }
+  res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next) => {
+  const nm = req.body.name;
+  const ml = req.body.mail;
+  const ag = req.body.age;
+  db.serialize(() => {
+    db.run('insert into mydata (name, mail, age) values (?, ?, ?)',
+      nm, ml, ag);
+  });
+  res.redirect('/hello');
+});
+
+router.get('/show', (req, res, next) => {
+  const id = req.query.id;
+  db.serialize(() => {
+    const q = "select * from mydata where id = ?";
+    db.get(q, [id], (err, row) => {
+      if (!err) {
+        var data = {
+          title: 'Hello/show',
+          content: 'id = ' + id + ' のレコード：',
+          mydata: row
+        }
+        res.render('hello/show', data);
+      }   
+    }); 
+  }); 
+});
+
+router.get('/edit', (req, res, next) => {
+  const id = req.query.id;
+  db.serialize(() => {
+    const q = "select * from mydata where id = ?";
+    db.get(q, [id], (err, row) => {
+      if (!err) {
+        var data = {
+          title: 'hello/edit',
+          content: 'id = ' + id + ' のレコードを編集：',
+          mydata: row
+        }
+        res.render('hello/edit', data);
+      }   
+    }); 
+  }); 
+});
+
+router.post('/edit', (req, res, next) => {
+  const id = req.body.id;
+  const nm = req.body.name;
+  const ml = req.body.mail;
+  const ag = req.body.age;
+  const q = "update mydata set name = ?, mail = ?, age = ? where id = ?";
+  db.serialize(() => {
+    db.run(q, nm, ml, ag, id);
+  });
+  res.redirect('/hello');
+});
+
+router.get('/delete', (req, res, next) => {
+  const id = req.query.id;
+  db.serialize(() => {
+    const q = "select * from mydata where id = ?";
+    db.get(q, [id], (err, row) => {
+      if (!err) {
+        var data = {
+          title: 'Hello/Delete',
+          content: 'id = ' + id + ' のレコードを削除：',
+          mydata: row
+        }
+        res.render('hello/delete', data);
+      }   
+    }); 
+  }); 
+});
+
+router.post('/delete', (req, res, next) => {
+  const id = req.body.id;
+  db.serialize(() => {
+    const q = "delete from mydata where id = ?";
+    db.run(q, id);
+  });
+  res.redirect('/hello');
+});
+
+router.get('/find',(req, res, next) => {
+  db.serialize(() => {
+    db.all("select * from mydata",(err, rows) => {
+      if (!err) {
+        var data = {
+          title: 'Hello/find',
+          find:'',
+          content:'検索条件を入力して下さい。',
+          mydata: rows
+        };
+        res.render('hello/find', data);
+      }   
+    }); 
+  }); 
+});
+
+router.post('/find', (req, res, next) => {
+  var find = req.body.find;
+  db.serialize(() => {
+    var q = "select * from mydata where ";
+    db.all(q + find, [], (err, rows) => {
+      if (!err) {
+        var data = {
+          title: 'Hello/find',
+          find:find,
+          content: '検索条件 ' + find,
+          mydata: rows
+        }
+        res.render('hello/find', data);
+      } 
+    }); 
+  }); 
+});
+
+module.exports = router;
